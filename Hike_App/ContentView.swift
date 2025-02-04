468b43b46c5c14a71a1a93cccf0334e99fff180f
Diego Herreros <diegohp85@gmail.com>
2025-02-04 18:23:59 +0100

    Initial Commit

---
+++ Hike_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike_App
+//
+//  Created by Diego Herreros Parron on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike_App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike_App
+//
+//  Created by Diego Herreros Parron on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
